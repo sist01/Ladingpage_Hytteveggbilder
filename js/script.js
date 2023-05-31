15638127c29dfb988ce2d85dfe5738066a514439
Silje <silje.eidsvoll@gmail.com>
2023-05-31 13:28:17 +0200

    Trying to set in alert on click button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-function downloadApp() {
+
+
+  function downloadApp() {
     // Handle download logic here
     console.log('Downloading app...');
+  
+    // Show alert message
+   
+    function myFunction() {
+      alert("Hello! I am an alert box!");
+    }
+    
   }
